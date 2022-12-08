d06a218b5b99a71bbc078195bab8ca7b569d287e
gaurav013 <abhinavsonone@protonmail.com>
2022-12-08 23:18:10 +0530

    Java Version incompatibility issues

--- order-services/src/main/java/com/cerillion/os/api/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cerillion.os.api.repository;
-
-import com.cerillion.os.api.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-
-}
